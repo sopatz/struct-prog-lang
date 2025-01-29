5c146f74fb9403180974b5c78f83d86489cd03e6
Seth Opatz <143544109+sopatz@users.noreply.github.com>
2025-01-29 22:03:11 +0000

    parser update

--- topic-02-PMDAS/parser.py
+++ topic-02-PMDAS/parser.py
@@ -1,3 +1,5 @@
+from tokenizer import tokenize
+
 """
 parser.py -- implement parser for simple expressions
 
@@ -13,3 +15,94 @@ Accept a string of tokens, return an AST expressed as stack of dictionaries
     boolean_expression == boolean_term { "||" boolean_term }
     expression = boolean_expression
 """
+
+def parse_factor(tokens):
+    """
+    factor = <number>
+    """
+    token = tokens[0]
+    if token["tag"] == "number":
+        return {
+            "tag":"number",
+            "value":token['value']
+        }, tokens[1:]
+    raise Exception(f"Unexpected token '{token['tag']} at position {token['position']}")
+
+def test_parse_factor():
+    """
+    factor = <number>
+    """
+    print("testing parse_factor()")
+    for s in ["1", "22", "333"]:
+        tokens = tokenize(s)
+        ast, tokens = parse_factor(tokens)
+        assert ast == {'tag': 'number', 'value': int(s)}
+        assert tokens[0]['tag'] == None
+    #print(ast)
+    #print(tokens)
+
+def parse_term(tokens):
+    """
+    term = factor { "*"|"/" factor }
+    """
+    node, tokens = parse_factor(tokens) # node = part of ast (part of tree)
+    while tokens[0]["tag"] in ["*", "/"]:
+        tag = tokens[0]["tag"]
+        tokens = tokens[1:]
+        right_node, tokens = parse_factor(tokens)
+        node = {"tag":tag, "left":node, "right":right_node}
+
+    return node, tokens
+
+def test_parse_term():
+    """
+    term = factor { "*"|"/" factor }
+    """
+    print("testing parse_term()")
+    for s in ["1", "22", "333"]:
+        tokens = tokenize(s)
+        ast, tokens = parse_term(tokens)
+        assert ast == {'tag': 'number', 'value': int(s)}
+        assert tokens[0]['tag'] == None
+    tokens = tokenize("2*4")
+    ast, tokens = parse_term(tokens)
+    assert ast == {'tag': '*', 'left': {'tag': 'number', 'value': 2}, 'right': {'tag': 'number', 'value': 4}}
+    tokens = tokenize("2*4/6")
+    ast, tokens = parse_term(tokens)
+    assert ast == {'tag': '/', 'left': {'tag': '*', 'left': {'tag': 'number', 'value': 2}, 'right': {'tag': 'number', 'value': 4}}, 'right': {'tag': 'number', 'value': 6}}
+
+def parse_expression(tokens):
+    """
+    term = factor { "*"|"/" factor }
+    """
+    node, tokens = parse_term(tokens) # node = part of ast (part of tree)
+    while tokens[0]["tag"] in ["+", "-"]:
+        tag = tokens[0]["tag"]
+        tokens = tokens[1:]
+        right_node, tokens = parse_term(tokens)
+        node = {"tag":tag, "left":node, "right":right_node}
+
+    return node, tokens
+
+def test_parse_expression():
+    """
+    term = factor { "*"|"/" factor }
+    """
+    print("testing parse_expression()")
+    for s in ["1", "22", "333"]:
+        tokens = tokenize(s)
+        ast, tokens = parse_expression(tokens)
+        assert ast == {'tag': 'number', 'value': int(s)}
+        assert tokens[0]['tag'] == None
+    tokens = tokenize("2*4")
+    ast, tokens = parse_expression(tokens)
+    assert ast == {'tag': '*', 'left': {'tag': 'number', 'value': 2}, 'right': {'tag': 'number', 'value': 4}}
+    tokens = tokenize("1+2*4")
+    ast, tokens = parse_expression(tokens)
+    assert ast == {'tag': '+', 'left': {'tag': 'number', 'value': 1}, 'right': {'tag': '*', 'left': {'tag': 'number', 'value': 2}, 'right': {'tag': 'number', 'value': 4}}}
+
+if __name__ == "__main__":
+    test_parse_factor()
+    test_parse_term()
+    test_parse_expression()
+    print("Done testing.")
